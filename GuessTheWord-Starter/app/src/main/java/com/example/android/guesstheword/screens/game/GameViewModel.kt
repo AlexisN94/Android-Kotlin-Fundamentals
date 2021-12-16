e0ed2f0e4129a011f1fc12449f537729af888747
Alexis Navarro <alexisnavarro@gmx.com>
2021-12-16 20:13:35 +0000

    Adds a CountDownTimer with LiveData transformation

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,36 +1,76 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
+    }
 
-    // The current _word
+    // The current word
     private val _word = MutableLiveData<String>()
-
-    // The current _score
-    private val _score = MutableLiveData<Int>()
-
-    // Event which triggers the end of the game
-    private val _eventGameFinish = MutableLiveData<Boolean>()
-
-    val score: LiveData<Int>
-        get() = _score
-
     val word: LiveData<String>
         get() = _word
 
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
+    // Event which triggers the end of the game
+    private val _eventGameFinish = MutableLiveData<Boolean>()
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
+
     // The list of words - the front of the list is the next _word to guess
     private lateinit var wordList: MutableList<String>
+
+    // Countdown timer
+    private val timer: CountDownTimer
+
+    // Countdown time
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val timeLeftString = Transformations.map(currentTime){ time ->
+
+        Log.d("GameViewModel", time.toString())
+
+        /* Doesn't work properly...
+         DateUtils.formatElapsedTime(time)
+        */
+
+        var minutesLeft = (time/1000)/60
+        var secondsLeft = (time/1000)%60
+        "$minutesLeft:$secondsLeft"
+    }
 
     init {
         _word.value = ""
         _score.value = 0
         _eventGameFinish.value = false
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(msLeft: Long) {
+               _currentTime.value = msLeft
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }.start()
+
         resetList()
         nextWord()
     }
@@ -67,7 +107,7 @@ class GameViewModel : ViewModel() {
 
     /** Methods for buttons presses **/
     fun onSkip() {
-        _score.value = _score.value?.plus(1)
+        _score.value = _score.value?.minus(1)
         nextWord()
     }
 
@@ -84,7 +124,7 @@ class GameViewModel : ViewModel() {
      * Moves to the next _word in the list
      */
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
+        if (wordList.isNotEmpty()) {
             //Select and remove a _word from the list
             _word.value = wordList.removeAt(0)
         } else {
@@ -94,5 +134,10 @@ class GameViewModel : ViewModel() {
 
     fun onGameFinishComplete() {
         _eventGameFinish.value = false
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
     }
 }
